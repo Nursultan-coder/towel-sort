e8f37bef0df94fb31c9a6dabc6319d1853dfeef7
Nursultan <65071183+Nursultan-coder@users.noreply.github.com>
2021-02-02 00:37:05 +0600

    штвучюоы

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+  matrix.forEach(item => {
+    item.forEach(item => {
+      result[item - 1] = item;
+    });
+  });
+  return result;
 }
